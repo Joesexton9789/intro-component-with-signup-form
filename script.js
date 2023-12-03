b158161582f39a7c41785c59765d0c2ea80a9aa4
Joe Sexton <joeysexton97@gmail.com>
2023-12-03 18:55:27 -0500

    updated javascript

--- script.js
+++ script.js
@@ -4,19 +4,29 @@ const firstName = document.getElementById('first_name')
 const lastName = document.getElementById('last_name')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
-const formControl = document.querySelectorAll('.form-control')
-const smallTags = document.querySelectorAll('small')
 
-function onSubmit(e) {
-  e.preventDefault()
+function showError(value, message) {
+  const label = value.parentElement
+  const small = label.querySelector('small')
+  if (value.value === '') {
+    small.innerText = message
+  } else {
+    small.innerText = ''
+  }
+}
 
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-  const svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+function hasError(input) {
+  input.forEach(values => {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    const svgPath = document.createElementNS(
+      'http://www.w3.org/2000/svg',
+      'path'
+    )
 
-  svg.setAttribute('width', '24')
-  svg.setAttribute('height', '24')
+    svg.setAttribute('width', '24')
+    svg.setAttribute('height', '24')
 
-  svg.innerHTML = `
+    svg.innerHTML = `
   <g fill="none" fill-rule="evenodd">
   <circle fill="#FF7979" cx="12" cy="12" r="12" />
   <rect
@@ -38,21 +48,34 @@ function onSubmit(e) {
 </g>
   `
 
-  svg.appendChild(svgPath)
+    svg.appendChild(svgPath)
 
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].value === '') {
-      console.log('empty')
-      inputs[i].insertBefore(svg, inputs[i].nextSibling)
+    if (values.value === '') {
+      values.insertAdjacentElement('afterend', svg)
+      values.style.borderColor = 'red'
     }
-  }
+  })
+}
 
-  // inputs.forEach(input => {
-  //   console.log(input.value)
-  //   if (input.value === '') {
-  //     input.insertAdjacentElement('afterend', svg)
-  //   }
-  // })
+function checkEmail(value) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+  if (!re.test(value.value.trim())) {
+    showError(email, 'Looks like this is not an email')
+  } else {
+    showError(email, '')
+  }
+}
+
+function onSubmit(e) {
+  e.preventDefault()
+
+  hasError([firstName, lastName, password, email])
+  showError(firstName, 'First name cannot be empty')
+  showError(lastName, 'Last name cannot be empty')
+  showError(password, 'Password cannot be empty')
+  checkEmail(email)
 }
 
 form.addEventListener('submit', onSubmit)

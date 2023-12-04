1e5008fefc4dc2222fc8f75162d8c7278bc5320d
Joe Sexton <joeysexton97@gmail.com>
2023-12-03 19:35:21 -0500

    trying to get svgs to go away after inputs are correct

--- script.js
+++ script.js
@@ -8,25 +8,13 @@ const password = document.getElementById('password')
 function showError(value, message) {
   const label = value.parentElement
   const small = label.querySelector('small')
-  if (value.value === '') {
-    small.innerText = message
-  } else {
-    small.innerText = ''
-  }
-}
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  const svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
 
-function hasError(input) {
-  input.forEach(values => {
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-    const svgPath = document.createElementNS(
-      'http://www.w3.org/2000/svg',
-      'path'
-    )
+  svg.setAttribute('width', '24')
+  svg.setAttribute('height', '24')
 
-    svg.setAttribute('width', '24')
-    svg.setAttribute('height', '24')
-
-    svg.innerHTML = `
+  svg.innerHTML = `
   <g fill="none" fill-rule="evenodd">
   <circle fill="#FF7979" cx="12" cy="12" r="12" />
   <rect
@@ -48,13 +36,20 @@ function hasError(input) {
 </g>
   `
 
-    svg.appendChild(svgPath)
+  svg.appendChild(svgPath)
+  if (value.value === '') {
+    small.innerText = message
 
-    if (values.value === '') {
-      values.insertAdjacentElement('afterend', svg)
-      values.style.borderColor = 'red'
-    }
-  })
+    value.insertAdjacentElement('afterend', svg)
+    value.style.borderColor = 'red'
+  }
+
+  if (value.value !== '') {
+    small.innerText = ''
+
+    value.nextElementSibling.remove(svg)
+    value.style.borderColor = 'hsl(246, 25%, 77%)'
+  }
 }
 
 function checkEmail(value) {
@@ -71,7 +66,6 @@ function checkEmail(value) {
 function onSubmit(e) {
   e.preventDefault()
 
-  hasError([firstName, lastName, password, email])
   showError(firstName, 'First name cannot be empty')
   showError(lastName, 'Last name cannot be empty')
   showError(password, 'Password cannot be empty')
